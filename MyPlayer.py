8b039a36b1af9a619d2f06adc9bacfa25cca449b
ARTEM775632 <89517490284@mail.ru>
2022-10-20 17:12:10 +0300

    Initial commit

---
+++ MyPlayer.py
@@ -0,0 +1,86 @@
+import pygame
+from constants import *
+
+class Player():
+    def __init__(self, name: str):
+        self.state = ALIVE
+        self.x = START_X
+        self.y = START_Y
+        self.direction = RIGHT
+        self.name = name
+        self.hp = MAX_HP
+        self.mp = MAX_MP
+        self.mooving = [0, 0, 0, 0]
+        self.frame = 0
+        self.walk = False
+        #self.image_pack = ['data/archerr.png','data/archerd.png','data/archerl.png','data/archeru.png']
+        # self.images = []
+        # for image in self.image_pack:
+        #     temp = pygame.image.load(image).convert_alpha()
+        #     i=[]
+        #     i.append(temp.subsurface(0,0,64,64))
+        #     i.append(temp.subsurface(64, 0, 64, 64))
+        #     i.append(temp.subsurface(128, 0, 64, 64))
+        #     self.images.append(i)
+        temp = pygame.image.load('data/wizard.png').convert_alpha()
+        i = []
+        i.append(temp.subsurface(240, 186, 60, 88))
+        i.append(temp.subsurface(0, 0, 60, 88))
+        i.append(temp.subsurface(0, 363, 59, 87))
+        i.append(temp.subsurface(300, 0, 60, 88))
+        i.append(temp.subsurface(60, 0, 60, 88))
+        self.image = i
+
+
+
+        i = []
+        for j in range(8):
+            i.append(temp.subsurface(j*60, 186, 60, 88))
+        #i.append(temp.subsurface(60, 188, 60, 88))
+        #i.append(temp.subsurface(120, 190, 60, 88))
+        #i.append(temp.subsurface(180, 190, 60, 88))
+        #i.append(temp.subsurface(60, 0, 60, 88))
+        self.right_images = i
+
+        for k in range(8):
+            i.append(temp.subsurface(k*60, 124, 60, 88))
+        #i.append(temp.subsurface(60, 188, 60, 88))
+        #i.append(temp.subsurface(120, 190, 60, 88))
+        #i.append(temp.subsurface(180, 190, 60, 88))
+        #i.append(temp.subsurface(60, 0, 60, 88))
+        self.left_images = i
+
+    def moove(self):
+
+
+        if self.mooving[RIGHT] == 1:
+            self.x+=PLAYER_SPEED
+        if self.mooving[DOWN]:
+            self.y+=PLAYER_SPEED
+        if self.mooving[LEFT]:
+            self.x-=PLAYER_SPEED
+        if self.mooving[UP]:
+            self.y-=PLAYER_SPEED
+
+        if self.x <= 0: self.x = 0
+        if self.y <= 0: self.y = 0
+        if self.x >= SCREEN_WIDTH-35: self.x = SCREEN_WIDTH-35
+        if self.y >= SCREEN_HEIGHT-75: self.y = SCREEN_HEIGHT-75
+
+
+    def render(self, screen):
+        # screen.blit(self.images[self.direction][self.state], (self.x, self.y))
+        #Animatingd
+        if self.walk:
+            self.frame += FRAME_PRAIM
+            if int(self.frame) == 8:
+                self.frame = 0
+            if self.direction == RIGHT:
+                self.image[self.direction]= self.right_images[int(self.frame)]
+            if self.direction == LEFT:
+                self.image[self.direction]= self.left_images[int(self.frame)]
+        screen.blit(self.image[self.direction], (self.x, self.y))
+    def render_ui(self, screen):
+        pass
+
+
---
+++ mymain.py
@@ -0,0 +1,82 @@
+import pygame
+from pygame import *
+from constants import *
+from MyPlayer import Player
+
+class Main():
+    def __init__(self, screen):
+        self.screen = screen
+        self.player = Player('Grut')
+        self.background = pygame.image.load('data/background.jpg')
+        self.running = True
+        self.main_loop()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == KEYDOWN:
+                if event.key == K_RIGHT:
+                    self.player.walk=True
+                    self.player.direction = RIGHT
+                    self.player.mooving = [1,0,0,0]
+                if event.key == K_DOWN:
+                    self.player.walk = True
+                    self.player.direction = DOWN
+                    self.player.mooving = [0,1,0,0]
+                if event.key == K_LEFT:
+                    self.player.walk = True
+                    self.player.direction = LEFT
+                    self.player.mooving = [0,0,1,0]
+                if event.key == K_UP:
+                    self.player.walk = True
+                    self.player.direction = UP
+                    self.player.mooving = [0,0,0,1]
+
+                if event.key == K_SPACE:
+                    if self.player.state != DEAD:
+                        self.player.state = DEAD
+                    else:
+                        self.player.state = ALIVE
+
+                if event.key == K_z:
+                    if self.player.state != SHOOT:
+                        self.player.state = SHOOT
+                    else:
+                        self.player.state = ALIVE
+
+
+
+            elif event.type == KEYUP:
+                if event.key == K_RIGHT:
+                    self.player.walk = False
+                    self.player.mooving[RIGHT] = 0
+                if event.key == K_DOWN:
+                    self.player.walk = False
+                    self.player.mooving[DOWN] = 0
+                if event.key == K_LEFT:
+                    self.player.walk = False
+                    self.player.mooving[LEFT] = 0
+                if event.key == K_UP:
+                    self.player.walk = False
+                    self.player.mooving[UP] = 0
+
+
+    def render(self):
+
+        self.screen.blit(self.background,(0,0))
+        self.player.render(screen)
+        pygame.display.flip()
+
+    def main_loop(self):
+        while self.running == True:
+            if self.player.state != DEAD:
+                self.player.moove()
+            self.render()
+            self.handle_events()
+            clock.tick(200)
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+game = Main(screen)
